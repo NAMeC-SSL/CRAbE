b46cdcf8ee8cb599cb14181ddac5725955384005
Jossl123 <jossl1.gau@gmail.com>
2023-03-15 17:03:00 +0100

    add position of goal

--- crates/crabe_framework/src/data/geometry/goal.rs
+++ crates/crabe_framework/src/data/geometry/goal.rs
@@ -13,4 +13,20 @@ pub struct Goal {
     pub top_left_position: Point2<f64>,
 }
 
-impl Goal {}
+impl Goal {
+    pub fn top_center_position(&self) -> Point2<f64>{
+        let goal_x = self.top_left_position.x- self.depth/2.;
+        let goal_y = self.top_left_position.y - self.width/2.;
+        Point2::new(goal_x, goal_y)
+    }
+    pub fn bottom_left_position(&self) -> Point2<f64>{
+        let goal_x = self.top_left_position.x- self.depth;
+        let goal_y = self.top_left_position.y;
+        Point2::new(goal_x, goal_y)
+    }
+    pub fn bottom_right_position(&self) -> Point2<f64>{
+        let goal_x = self.top_left_position.x- self.depth;
+        let goal_y = self.top_left_position.y - self.width;
+        Point2::new(goal_x, goal_y)
+    }
+}
