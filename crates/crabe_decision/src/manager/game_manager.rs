cbe036f21a269725335c24e705b0faa4798e50fd
Jossl123 <jossl1.gau@gmail.com>
2023-06-26 17:39:17 +0200

    keeper strategy : just follow y of the ball

--- crates/crabe_decision/src/manager/game_manager.rs
+++ crates/crabe_decision/src/manager/game_manager.rs
@@ -2,6 +2,7 @@ use crate::action::ActionWrapper;
 use crate::manager::Manager;
 use crate::strategy::Strategy;
 use crate::strategy::testing::Square;
+use crate::strategy::keeper::Keep;
 use crabe_framework::data::tool::ToolData;
 use crabe_framework::data::world::game_state::{GameState, RunningState, StoppedState};
 use crabe_framework::data::world::World;
@@ -75,6 +76,7 @@ impl Manager for GameManager {
                     RunningState::Run => {
                         println!("run");
                         self.strategies.push(Box::new(Square::new(1)));
+                        self.strategies.push(Box::new(Keep::new(0)));
                     }
                 },
             }
